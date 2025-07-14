9eab8a481f6932d0af9cbecab6391d0be9df639d
Mariia <shymanskam524@gmail.com>
2025-07-14 23:27:58 +0200

    add RecepiPreview

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
-import { store } from './redux/store.js';
-import { Provider } from 'react-redux';
-import App from './components/App/App.jsx';
-import 'normalize.css';
-import 'izitoast/dist/css/iziToast.min.css';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import App from "./components/App/App.jsx";
+// import "normalize.css";
+import "izitoast/dist/css/iziToast.min.css";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
-    </StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 );
