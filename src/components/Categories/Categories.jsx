064b536eca2d217d924cae25555da93ba565bf6c
Volodymyr Antoniuk <voxtobox@gmail.com>
2025-07-14 21:49:43 -0700

    fix categories

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,19 +1,22 @@
 import styles from './Categories.module.css';
 import CategoryList from '../CategoryList/CategoryList';
+import MainTitle from '../common/ui/MainTitle/MainTitle';
+import Subtitle from '../common/ui/Subtitle/Subtitle';
 
 const Categories = ({ onCategorySelect }) => {
-  return (
-    <section className={styles.categories}>
-      <div className={styles.header}>
-        <h2 className={styles.title}>Categories</h2>
-        <p className={styles.description}>
-          Discover a limitless world of culinary possibilities and enjoy exquisite 
-          recipes that combine taste, style and the warm atmosphere of the kitchen.
-        </p>
-      </div>
-      <CategoryList onCategorySelect={onCategorySelect} />
-    </section>
-  );
+    return (
+        <section className={styles.categories}>
+            <div className={styles.header}>
+                <MainTitle>CATEGORIES</MainTitle>
+                <Subtitle limitWidth>
+                    Discover a limitless world of culinary possibilities and
+                    enjoy exquisite recipes that combine taste, style and the
+                    warm atmosphere of the kitchen.
+                </Subtitle>
+            </div>
+            <CategoryList onCategorySelect={onCategorySelect} />
+        </section>
+    );
 };
 
-export default Categories; 
+export default Categories;
